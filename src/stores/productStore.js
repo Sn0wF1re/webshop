be17996a666a50a3a1b07cc8db589b796800cac5
Sn0wF1re <mokayaclinton34@gmail.com>
2024-07-27 01:22:14 +0300

    Modify api call
    modify endpoint to retrieve various relations and make the call dynamic

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -9,7 +9,7 @@ export const useProductStore = defineStore('productStore', () => {
 
     const fetchProducts = async () => {
         try {
-            const response = await axios.get('http://localhost:1337/api/products?populate[cover_photo][fields][0]=formats&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en', {
+            const response = await axios.get('http://localhost:1337/api/products?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en', {
                 headers: {
                     'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
                 }
@@ -24,13 +24,15 @@ export const useProductStore = defineStore('productStore', () => {
 
     const fetchProduct = async (id) => {
         try {
-            const response = await axios.get(`http://localhost:1337/api/products/1?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variation][fields][0]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en`, {
+            const response = await axios.get(`http://localhost:1337/api/products/${id}?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en`, {
                 headers: {
                     'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
                 }
             });
 
             product.value = response.data.data;
+            availableColors.value = product.value.product_variations.map(variation => variation.color);
+            availableSizes.value = product.value.product_variations.map(variation => variation.size);
             console.log(product.value);
 
         } catch (error) {
