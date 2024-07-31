951fbe2896311844c4332a797af3a2eab0b1cb18
Sn0wF1re <mokayaclinton34@gmail.com>
2024-07-31 15:45:13 +0300

    Modify product info card
    - section the details part to have two sections
    - include ratings with stars

--- src/components/ProductInfoCard.vue
+++ src/components/ProductInfoCard.vue
@@ -1,59 +1,65 @@
 <template>
-    <div class="product-info-card">
-      <img :src="product.attributes.cover_photo.data.attributes.formats.small.url" :alt="product.attributes.mask_id" />
-        <div class="product-info">
-            <p>category: {{ product.attributes.category.data.attributes.name }}</p>
-            <h3>{{ product.attributes.mask_id }}</h3>
-            <p>kes {{ product.attributes.price_kes }} / usd {{ product.attributes.price_usd }}</p>
-            <p> {{ product.attributes.description }}</p>
-
-            <div class="variations">
-              <label for="size">Size: </label>
-              <select name="size" id="size" v-model="selectedSize">
+  <div class="product-info-card">
+    <img :src="product.attributes.cover_photo.data.attributes.formats.small.url" :alt="product.attributes.mask_id" />
+    <div class="product-info">
+      <div class="left">
+        <h3>{{ product.attributes.mask_id }}</h3>
+        <q-rating size="18px" v-model="stars" readonly disable color="primary" />
+        <p>category: {{ product.attributes.category.data.attributes.name }}</p>
+        <p>kes {{ product.attributes.price_kes }} / usd {{ product.attributes.price_usd }}</p>
+  
+        <div class="variations">
+        <!-- <label for="size">Size: </label> -->
+        <!-- <select name="size" id="size" v-model="selectedSize">
                 <option 
                 v-for="size in availableSizes" 
                 :value="size"
                 >
                 {{  size }}
                 </option>
-              </select>
+              </select> -->
 
-              <label for="color">Color: </label>
-              <select name="color" id="color" v-model="selectedColor">
+        <q-select v-model="selectedSize" :options="availableSizes" label="Size" stack-label />
+
+        <!-- <label for="color">Color: </label> -->
+        <!-- <select name="color" id="color" v-model="selectedColor">
                 <option 
                 v-for="color in availableColors" 
                 :value="color"
                 >
                 {{  color }}
                 </option>
-              </select>
-            </div>
-
-            <div class="quantity">
-              <label :for="'update-quantity-' + product.id">Quantity:</label>
-              <input type="number" v-model.number="itemQuantity"
-                placeholder="e.g 5" min="1"
-                :id="'update-quantity-' + product.id">
-              </input>
-            </div>
-            <div class="product-rating">
-              <span>
-                <!-- <i
-                v-for="i in 5"
-                :key="i"
-                :class="product.rating >= i ? 'fa fa-star' : 'fa fa-star-o'"
-                ></i> -->
-                rating
-              </span>
-            </div>
-            <div class="buttons">
-              <button @click="addToCart">Add to Cart</button>
-              <button>
-                <router-link to="/cart">View Cart</router-link>
-              </button>
-            </div>
+              </select> -->
+        <q-select v-model="selectedColor" :options="availableColors" label="Color" stack-label color="primary" />
         </div>
+  
+        <div class="quantity">
+          <!-- <label :for="'update-quantity-' + product.id">Quantity:</label> -->
+          <!-- <input type="number" v-model.number="itemQuantity"
+                  placeholder="e.g 5" min="1"
+                  :id="'update-quantity-' + product.id">
+                </input> -->
+          <q-input :id="'update-quantity-' + product.id" label="Quantity" v-model.number="itemQuantity" type="number"
+            min="1" style="max-width: 200px" square />
+        </div>
+      </div>
+    
+      <div class="right">
+        <div class="description">
+          <!-- <h3>Description</h3> -->
+          <p> {{ product.attributes.description }}</p>
+        </div>
+  
+        <div class="buttons">
+          <button @click="addToCart">Add to Cart</button>
+          <button>
+            <router-link to="/cart">View Cart</router-link>
+          </button>
+        </div>
+        
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -69,8 +75,8 @@ console.log(props.product);
 
 const cartStore = useCartStore();
 const {itemQuantity, selectedSize, selectedColor, cartCount, cartItems} = storeToRefs(cartStore);
-const availableSizes = new Set(props.product.attributes.product_variations.data.map((variation) => variation.attributes.size.data.attributes.type));
-const availableColors = new Set(props.product.attributes.product_variations.data.map((variation) => variation.attributes.color.data.attributes.name));
+const availableSizes = [...new Set(props.product.attributes.product_variations.data.map((variation) => variation.attributes.size.data.attributes.type))];
+const availableColors = [...new Set(props.product.attributes.product_variations.data.map((variation) => variation.attributes.color.data.attributes.name))];
 
 const addToCart = () => {
   cartStore.addToCart(props.product);
@@ -82,9 +88,9 @@ const addToCart = () => {
 <style scoped>
 .product-info-card {
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: flex-start;
-  gap: 3rem;
+  gap: 5rem;
   padding: 1rem;
   margin: 1rem;
   border: 1px solid #e6a97a;
@@ -93,8 +99,9 @@ const addToCart = () => {
 
   .product-info {
     display: flex;
-    flex-direction: column;
-    gap: 1rem;
+    justify-content: center;
+    align-items: center;
+    gap: 5rem;
     margin-left: 1rem;
     font-family: "Cambay", sans-serif;
 
@@ -107,54 +114,75 @@ const addToCart = () => {
       font-weight: bold;
     }
 
-    .variations {
-      label {
-        font-weight: bold;
+    .left {
+      .variations {
+        label {
+          font-weight: bold;
+        }
+    
+        select {
+          padding: 0.25rem;
+          border: 1px solid #e6a97a;
+          font-family: "Cambay", sans-serif;
+          outline: none;
+          text-align: center;
+        }
+    
+        option {
+          font-family: "Inter", sans-serif;
+          outline: none;
+          text-align: center;
+          font-weight: normal;
+        }
+    
+        #size {
+          margin-right: 1rem;
+        }
       }
-
-      select {
-        padding: 0.25rem;
-        border: 1px solid #e6a97a;
-        font-family: "Inter", sans-serif;
-        outline: none;
-        text-align: center;
-      }
-
-      option {
-        font-family: "Inter", sans-serif;
-        outline: none;
-        text-align: center;
-        font-weight: normal;
-      }
-
-      #size {
-        margin-right: 1rem;
+      
+      .quantity {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    
+        label {
+          font-weight: bold;
+        }
+    
+        input {
+          padding: 0.5rem;
+          border: 1px solid #e6a97a;
+          font-family: "Inter", sans-serif;
+          outline: none;
+        }
       }
     }
 
-    .quantity {
+    .right {
       display: flex;
       flex-direction: column;
-      gap: 0.5rem;
-
-      label {
-        font-weight: bold;
-      }
-
-      input {
-        padding: 0.5rem;
-        border: 1px solid #e6a97a;
-        font-family: "Inter", sans-serif;
-        outline: none;
-      }
-    }
-
-    .buttons {
-      display: flex;
       justify-content: space-between;
+      /* gap: 1rem; */
+      font-family: "Cambay", sans-serif;
+  
+      h3 {
+        color: #E47E30;
+        font-size: 1.5rem;
+        padding: 0;
+        margin: 0;
+      }
+  
+      .buttons {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 1rem;
+        bottom: 0;
+      }
     }
   }
 
+  
   button {
     cursor: pointer;
     background-color: #E47E30;
