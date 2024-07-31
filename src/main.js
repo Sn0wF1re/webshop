f0e2beb2d9aa5d537a559d05bc1a786609697943
Sn0wF1re <mokayaclinton34@gmail.com>
2024-07-31 15:38:52 +0300

    configure quasar
    add quasar and configure brand colors

--- src/main.js
+++ src/main.js
@@ -10,6 +10,14 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from './App.vue'
 import router from './router'
 
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
+
 library.add(faCartShopping)
 library.add(faUser)
 
@@ -19,5 +27,23 @@ const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    config: {
+        brand: {
+            primary: '#e47e30',
+            secondary: '#5d2a42',
+            accent: '#9C27B0',
+      
+            dark: '#1d1d1d',
+            'dark-page': '#121212',
+      
+            positive: '#30b84f',
+            negative: '#e0021c',
+            info: '#31CCEC',
+            warning: '#ffc524'
+        }
+    }
+})
 
 app.mount('#app')
