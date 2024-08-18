52cff444557918b212c586475946cc846a8016a3
Sn0wF1re <mokayaclinton34@gmail.com>
2024-08-18 16:38:51 +0300

    Change url in the file to the live one
    create url variable
    change url to deployed strapi url
    move api token to .env

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -2,6 +2,8 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 import { ref } from 'vue';
 
+const strapiKey = import.meta.env.VITE_STRAPI_KEY;
+const baseURL = 'https://pm-webshop-strapi-aad213b0148f.herokuapp.com';
 
 export const useProductStore = defineStore('productStore', () => {
     const products = ref([]);
@@ -9,9 +11,10 @@ export const useProductStore = defineStore('productStore', () => {
 
     const fetchProducts = async () => {
         try {
-            const response = await axios.get('http://localhost:1337/api/products?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en', {
+            const response = await axios.get(`${baseURL}/api/products?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en`, {
                 headers: {
-                    'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
+                    // 'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
+                    'Authorization': `Bearer ${strapiKey}`
                 }
             });
 
@@ -24,9 +27,10 @@ export const useProductStore = defineStore('productStore', () => {
 
     const fetchProduct = async (id) => {
         try {
-            const response = await axios.get(`http://localhost:1337/api/products/${id}?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en`, {
+            const response = await axios.get(`${baseURL}/api/products/${id}?populate[cover_photo][fields][0]=formats&populate[category][fields][0]=name&populate[product_variations][populate]=size&populate[product_variations][populate]=color&pagination[pageSize]=10&pagination[page]=1&publicationState=live&locale[0]=en`, {
                 headers: {
-                    'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
+                    // 'Authorization': 'Bearer 1e6521e8f6300e448d0961e515aa83d8333be1f5625e09b6cd950b13543e5b68f85c650b2687d1a3b3e4558286649c4ccee841884d45409cb1f0fdb2ea4e112cf61d03f2d47bed21544a5444784ad094ff459f37ba65f19aa8b615d73b93585fb05638fa640bedb7b54dca00053ea4b7762dc43b0c2616577f4d02e0cdccfad0'
+                    'Authorization': `Bearer ${strapiKey}`
                 }
             });
 
